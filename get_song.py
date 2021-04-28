a94d6f07faf6ce6d2fd39ffd76241fb873c3cb76
ksyasuda <ksyasuda@umich.edu>
2021-04-28 00:52:53 -0400

    fix filenotfound error

--- get_song.py
+++ get_song.py
@@ -25,24 +25,29 @@ class Music:
         self.data = {}
 
 
-    def get_artists_in_filesystem(self, path: pathlib.Path) -> list[str]:
+    def get_artists_in_filesystem(self, path: pathlib.Path, is_verbose: bool) -> list[str]:
         """
         returns a lsit of artists in ~/Music/
         """
+        if is_verbose:
+            print('Getting list of artists.')
         tlist = []
         for tpath in path.iterdir():
             if tpath.is_dir():
                 tlist.append(get_current_directory(tpath))
-                artist = get_current_directory(tpath)
-                if artist not in self.data:
-                    self.data[artist] = {}
+                temp = get_current_directory(tpath)
+                if temp not in self.data:
+                    self.data[temp] = {}
         return tlist
 
 
-    def get_albums_in_filesystem(self, path: pathlib.Path, artist: str):
+    def get_albums_in_filesystem(self, path: pathlib.Path, artist: str,
+                                 is_verbose: bool):
         """
         Returns list of albums in filesystem for given artist
         """
+        if is_verbose:
+            print(f'Getting list of albums for {artist}.')
         tlist: list[str] = []
         new_path = path.joinpath(artist)
         for i in new_path.iterdir():
@@ -54,16 +59,22 @@ class Music:
         return tlist
 
 
-    def check_album_exists(self, artist, alb_name: str) -> bool:
+    def check_album_exists(self, artist: str, alb_name: str, is_verbose: bool) -> bool:
         """
         Returns whether or not the album exists in the filesystem at ~/Music/Artist/
         """
         base_dir = pathlib.Path(config.BASE_DIR)
-        artists = self.get_artists_in_filesystem(base_dir)
+        if is_verbose:
+            print(f"Checking whether {alb_name} exists in {base_dir}.")
+        artists = self.get_artists_in_filesystem(base_dir, is_verbose)
         if artist in artists:
-            albums = self.get_albums_in_filesystem(base_dir, artist)
+            albums = self.get_albums_in_filesystem(base_dir, artist, is_verbose)
             if alb_name in albums:
+                if is_verbose:
+                    print(f"Found {alb_name} in directory.")
                 return True
+        if is_verbose:
+            print(f"Did not find {alb_name} in directory.")
         return False
 
 
@@ -91,27 +102,40 @@ def get_song(link: str) -> pathlib.Path:
     return path
 
 
-def create_dirs(artist_name: str, album_name: str) -> pathlib.Path:
+def create_dirs(artist_name: str, album_name: str, is_verbose: bool) -> pathlib.Path:
     """
     Creates directory for the artist and album and returns the path to the
     album.
     """
     base_dir = pathlib.Path(config.BASE_DIR)
     new_path = base_dir.joinpath(artist_name)
-    new_path.mkdir(parents=True)
-    new_path = new_path.joinpath(album_name)
-    new_path.mkdir(parents=True)
+    try:
+        if is_verbose:
+            print(f"Trying to create directory for {artist_name}")
+        new_path.mkdir(parents=True)
+        new_path = new_path.joinpath(album_name)
+        if is_verbose:
+            print('Creating new directory for {album_name} in {artist_name}')
+        new_path.mkdir(parents=True)
+    except FileExistsError:
+        if is_verbose:
+            print(f"{artist_name} already exists in filesystem...")
+        new_path = new_path.joinpath(album_name)
+        if is_verbose:
+            print('Creating new directory for {album_name} in {artist_name}')
+        new_path.mkdir(parents=True)
     return new_path
 
 
-def move_song(old_path: pathlib.Path, new_path: pathlib.Path):
+def move_song(old_path: pathlib.Path, new_path: pathlib.Path, is_verbose: bool):
     """
     Move song from download location to the passed in path.
     """
     song = get_current_directory(old_path)
     new_path = new_path.joinpath(song)
     old_path.rename(new_path)
-    print(f'Moved {song} to {new_path}')
+    if is_verbose:
+        print(f'Moved {song} to {new_path}')
 
 
 def get_space_separated(tname: str) -> str:
@@ -128,17 +152,18 @@ def get_space_separated(tname: str) -> str:
     return name
 
 
-def get_cover(art_name, alb_name):
+def get_cover(art_name: str, alb_name: str, is_verbose: bool):
     """
     Gets the album cover for a given album by artists.
     """
     aname = get_space_separated(art_name)
     bname = get_space_separated(alb_name)
-    print(f"Getting cover for {aname} {bname}")
     base_dir = config.BASE_DIR
     path = pathlib.Path(base_dir)
     out_path = str(path.joinpath(art_name, alb_name, 'cover.jpg'))
     command = f"sacad '{aname}' '{bname}' 1000 '{out_path}'"
+    if is_verbose:
+        print(f"Getting cover for {aname} {bname}")
     os.system(command)
 
 
@@ -157,25 +182,26 @@ def usage():
     print('-h, --help\t\thelp menu (this menu)')
 
 
-def run(song_link: str, artist_name: str, album_name: str):
+def run(song_link: str, artist_name: str, album_name: str, is_verbose: bool):
     """Gets the song with youtube-dl and creates directory if doesn't exist."""
     music = Music()
     temp_path = get_song(song_link)
-    has_album = music.check_album_exists(artist_name, album_name)
-    has_album = False
+    has_album = music.check_album_exists(artist_name, album_name, is_verbose)
     if not has_album:
-        path = create_dirs(artist_name, album_name)
-        move_song(temp_path, path)
-        get_cover(artist_name, album_name)
+        path = create_dirs(artist_name, album_name, is_verbose)
+        move_song(temp_path, path, is_verbose)
+        get_cover(artist_name, album_name, is_verbose)
         update_database()
     else:
         print(config.AlBUM_EXISTS.format(album_name))
+        move_song(temp_path, path, is_verbose)
+        update_database()
 
 
 if __name__ == '__main__':
-    args = ['help=', 'link=', 'name=', 'album=', 'help=', 'cover=']
+    args = ['link=', 'name=', 'album=', 'cover=', 'help=', 'verbose=']
     try:
-        options, remainder = gnu_getopt(sys.argv[1:], 'l:n:a:c:h', args)
+        options, remainder = gnu_getopt(sys.argv[1:], 'l:n:a:c:hv:', args)
         if len(options) == 0:
             usage()
             sys.exit(1)
@@ -184,6 +210,7 @@ if __name__ == '__main__':
         ALBUM = ''
         NEED_HELP=''
         COVER=''
+        VERBOSE=False
         for opt, arg in options:
             if opt in ('-h', '--help'):
                 usage()
@@ -196,7 +223,13 @@ if __name__ == '__main__':
                 ALBUM = arg
             elif opt in ('-c', '--cover'):
                 COVER = arg
-        run(LINK, NAME, ALBUM)
+            elif opt in ('-v', '--verbose'):
+                VERBOSE = True
+        if NAME == '' or ALBUM == '':
+            usage()
+            sys.exit(1)
+        run(LINK, NAME, ALBUM, VERBOSE)
+
     except GetoptError:
         print('Getopt Error')
         sys.exit(1)
